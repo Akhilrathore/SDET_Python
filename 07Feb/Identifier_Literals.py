9209e77cc0a728f9ebc841e7ec78b4fb2896f42f
Akhilrathore <Akhilrathore17@gmail.com>
2024-02-12 19:05:31 +0530

    12feb

---
+++ 07Feb/Identifier_Literals.py
@@ -0,0 +1,28 @@
+# Variable/Identifier = Value/Literals
+
+name = "Akhil"
+age = 30
+age1 =30
+print("age:",age is age1)
+pi = 3.14
+is_married = True
+have_car = None
+my_list = []
+my_set = {}
+newline = '\n'
+forTab = '\t'
+forBell = '\a'
+
+print(not is_married)
+
+# Numbers
+# binary 0,1. to store/assigning
+a = 0b1010  # a=10
+
+# Octa number
+b = 0o171
+print(b)
+
+# Hexa number
+c = 0xaf
+print(c)

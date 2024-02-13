5208f4241a64a31738d9169d624b9a6da73a8342
Akhilrathore <akhilrathore17@gmail.com>
2024-02-13 23:21:11 +0530

    9FEB TASK- done

--- 09Feb/Palindrom.py
+++ 09Feb/Palindrom.py
@@ -1,9 +1,25 @@
-str = input("Enter string: ")
-tmp = 1
-if len(str) > 0:
-    for i in range(0, len(str)):
-     tmp[i] == str[l]
-    print("Factorial of ", num, " is: ", tmp)
+str1 = input("Enter string: ")
+str2 = ""
+n = len(str1) - 1
+for i in range(len(str1)):
+    str2 = str2 + str1[n - i]
+if str2 == str1:
+    print({str1}, " is a palindrome")
 else:
-    print("incorrect input")
+    print({str1}, " is not a palindrome")
+#     ------------------------------------------------
+flag = True
+for i in range(len(str1) // 2):
+    if str1[i] != str1[len(str1) - i - 1]:
+        flag = False
+        break
+if flag:
+    print({str1}, " is a palindrome")
+else:
+    print({str1}, " is not a palindrome")
 
+# ----------------------
+if str1 == str1[::-1]:
+    print({str1}, " is a palindrome")
+else:
+    print({str1}, " is not a palindrome")
